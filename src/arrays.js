1889ae40e2674aa693e6588a81000da376a3fef2
agentjimlam <jlam.5883@gmail.com>
2024-08-31 07:03:28 +0800

    did Q1 array.js

--- src/arrays.js
+++ src/arrays.js
@@ -16,12 +16,14 @@ for(let i=0 ;i<defaults.length; i++){
 /*Class discussion
 Array methods:
 Break out into groups and research how each of the array methods can be used and give an example.
-    1. push
-    2. pop
-    3. shift
-    4. unshift
-    5. splice
-    6. slice
+    1. push     //.push(), allows us to add items to the end of an array
+    2. pop      //.pop(), removes the last item of an array.
+    3. shift    // remove the first item from the array. All subsequent elements will shift down one place.
+    4. unshift  // Adds one or more elements to beginning of array and returns new length.
+    5. splice   // modifies an array in place by inserting, removing, and/or replacing array elements then returning an array of removed elements.
+    6. slice    // .slice() method in JavaScript returns a partial copy of an array, otherwise known as a shallow copy, without altering the original array.
+
+https://www.codecademy.com/resources/docs/javascript/arrays/splice
 */
 
 //Exercise:
@@ -35,3 +37,24 @@ Question 2:
 const arr1 = [15, 12, 11, 29, 5];
 const arr2 = [13, 2, 6, 7];
 */
+
+const grades = [50,60,70,80,90];
+let totalGlobal = 0;
+
+for (let i=0; i < grades.length; i++){
+    totalGlobal += grades[i];
+}
+
+const average = totalGlobal / grades.length;
+console.log(average);
+
+const arr1 = [15, 12, 11, 29, 5];
+const arr2 = [13, 2, 6, 7];
+
+/*
+const arr3 = arr1.push(...arr2);  
+console.log(arr3); // this prints 9 (no. of elements in array)
+*/
+
+let combinedArray = [...arr1, ...arr2]; // using spread operator to combine arrays
+console.log(combinedArray);
